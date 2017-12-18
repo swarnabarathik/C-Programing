a2848a16defd0712cfb70519172394ce327366f8
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-18 07:32:09 +0000

    Create variables.c

---
+++ Beginner/variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void) {
+	char a[]="Sachin Ramesh Tendulkar";
+	char b[]="Little Master";
+	long int c=18426;
+	float d=44.83;
+	int e=463;
+	printf("%s\n%s\n%ld\n%f\n%d\n",a,b,c,d,e);
+	return 0;
+}
