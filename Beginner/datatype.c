5b6329449569568e849e792078699d175b391a91
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-18 09:17:02 +0000

    Create datatype.c

---
+++ Beginner/datatype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+	int a=10;
+	float b=34.5;
+	char c='d';
+	printf(" size of a %d\n",sizeof(a));
+	printf(" size of b %d\n",sizeof(b));
+	printf(" size of c %d\n",sizeof(c));
+	return 0;
+}
