afdeda409d348f53a6a531b4a1738b46c692c25e
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-19 07:23:47 +0000

    Create ifelse.c

---
+++ players/ifelse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (void)
+{
+int num;
+printf("Enter the number");
+scanf("%d",&num);
+if (num<0)
+printf("The number is negative") ;
+else
+printf("The number is positive");
+return 0;
+}
