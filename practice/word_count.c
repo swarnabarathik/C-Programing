9eba4785b40bc9bfb4fba62a09a63a26fe91fb92
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2018-03-08 10:19:00 +0530

    Create word_count.c

---
+++ practice/word_count.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[1000];
+	int n,i,count=1;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==' ')
+		count++;
+	}
+	if(count>1)
+	printf("%d",count);
+  return 0;
+}
