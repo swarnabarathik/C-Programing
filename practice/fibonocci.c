1ec3c369ca153b0d2e1d941edd12571113c01a04
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2018-03-09 09:36:08 +0530

    Create fibonocci.c

---
+++ practice/fibonocci.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,first=0,second=1,next,j;
+	scanf("%d",&n);
+	for(j=1;j<=n;j++)
+	{
+		if(j<=1)
+		next=j;
+		else
+		{
+			next=first+second;
+			first=second;
+			second=next;
+		}
+		printf("%d",next);
+	}
+  return 0;
+} 
