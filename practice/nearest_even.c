77dc2945b2ff4613f7467729d3cd80988183d481
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2018-03-09 10:39:52 +0530

    Create nearest_even.c

---
+++ practice/nearest_even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,temp;
+	scanf("%d",&n);
+	temp=n;
+	n=n%10;
+	if(n=='0'||n=='2'||n=='6'||n=='8'||n=='4')
+	{
+		printf("%d",temp);
+	}
+	else
+	{
+		printf("%d",temp-1);
+	}
+ return 0;
+}
