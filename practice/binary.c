8d39461413b858907e327f49742bf15ce5f997d9
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2018-05-17 22:01:21 +0530

    Create binary.c

---
+++ practice/binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[100];
+	int i,b,n;
+	scanf("%s",a);
+	b=strlen(a);
+	if(b==8)
+	{
+		for(i=0;i<b;i++)
+			{
+				if(a[i]=='1'||a[i]=='0')
+				{
+				}
+				
+			}
+			printf("Yes");
+	}
+	else
+	printf("No");
+  return 0;
+}
